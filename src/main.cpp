5d4e20bab6a7446b0827056d587e33cc61e2e6c0
JO-GHub <joseph.obong.engineering@gmail.com>
2025-11-15 13:13:43 +0000

    circle code

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 using namespace std;
+#define PI 3.14
 
 // Base class
 class Shape
@@ -7,28 +8,42 @@ class Shape
 	public:
 
 	// pure virtual function providing interface framework.
-	virtual int getArea() = 0;
-	void setWidth(int w)
-	{
-		width = w;
-	}
-	void setHeight(int h)
-	{
-		height = h;
-	}
+	virtual float getArea() = 0;
+		void setWidth(int w)
+		{
+			width = w;
+		}
+		void setHeight(int h)
+		{
+			height = h;
+		}
+		void setRadius(float r)
+		{
+			radius = r;
+		}
 
 	protected:
-	int width;
-	int height;
-	};
+		int width;
+		int height;
+		float radius;
+		};
 
 class Triangle: public Shape
 {
 	public:
-	int getArea()
-	{
-		return (width * height)/2;
-	}
+		float getArea()
+		{
+			return (width * height)/2;
+		}
+};
+
+class Circle: public Shape
+{
+	public:
+		float getArea()
+		{
+			return PI*radius*radius;
+		}
 };
 
 int main(void)
@@ -38,5 +53,10 @@ int main(void)
 	Tri.setHeight(7);
 	// Print the area of the object.
 	cout << "Total Triangle area: " << Tri.getArea() << endl;
+
+	Circle circle;
+	circle.setRadius(5);
+	cout << "Total Circle area: " << circle.getArea() << endl;
+
 	return 0;
 }
